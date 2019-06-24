5ad2f24ec5ab3f2c1e2e27e728755be7f1185e09
Koszmarny <35725185+Koszmarny@users.noreply.github.com>
2019-06-24 22:17:38 +0200

    krypt

--- src/Faktoryzacja.java
+++ src/Faktoryzacja.java
@@ -1,19 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Faktoryzacja {
 
-    public static double NWD(long numberA, long numberB) {
-        while (numberA != numberB) {
-            if (numberA > numberB) {
-                numberA = numberA - numberB;
-            } else {
-                numberB = numberB - numberA;
-            }
+    public static double Euklides(double x, double y){
+        while (x != y){
+            if (x > y)
+                x -= y;
+            else
+                y -= x;
         }
-        return numberA;
+        return x;
     }
 
     public static void main(String[] args) {
-        double e = 2, d = 3, n = 5, s, t;
-        double ed = e*d;
-        ed-1=t* 2^s;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("e: ");
+        double e = scan.nextInt();
+        System.out.println("d: ");
+        double d = scan.nextInt();
+        System.out.println("n: ");
+        double n = scan.nextInt();
+        Random rand = new Random();
+        double t = e * d - 1;
+        double s = 0;
+        while (t % 2 == 0) {
+            t /= 2;
+            s++;
+        }
+        double a = 0;
+        do {
+            a = rand.nextInt(100);
+        } while (Euklides(a, n) != 1);
+        double c = a;
+        a = Math.pow(a, t) % n;
+        while (a != 1) {
+            c = a;
+            a = Math.pow(c, 2) % n;
+            if ((c != 1 % n) && (a == 1 % n)) {
+                break;
+            }
+        }
+
+        double p = Euklides(c - 1, n);
+        double q = Euklides(c + 1, n);
+        System.out.println("p="+p+"q="+q);
     }
 }
+
